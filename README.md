963e9572503c50034ba58e8583c74e62bf9bf76c
Sam Shull <181527+samshull@users.noreply.github.com>
2024-12-05 08:37:37 -0500

    Switch to static-server for better compatibility with tunnel

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # @layerart/serve-art
 
-A small tool for serving the files in a directory so that they can be displayed in an iframe on another domain and hot reload when a file is updated.
+A small tool for serving the files in a directory so that they can be displayed in an iframe on the Layer submission platform. This server creates a tunnel in order to provide security compatibility in the browser.
 
 ## Command line options
 
@@ -8,6 +8,7 @@ A small tool for serving the files in a directory so that they can be displayed 
 - --host,-h - Set the host for the server, default is 0.0.0.0 or 127.0.0.1.
 - --root,-r - Set the root directory for the server, default is cwd.
 - --sandbox,-s - Specify the sandbox url. Default: https://app.layer.com/submissions/generative/sandbox
+- --tunnel_host,-t - Specify the tunnel host. Default https://layerproxy.com
 
 ## Usage
 
--- index.cjs
+++ index.cjs
@@ -1,71 +1,54 @@
 #!/usr/bin/env node
 
 const { parseArgs } = require('node:util');
-const readline = require('node:readline/promises');
-const { stdin: input, stdout: output } = require('node:process');
 
-const liveServer = require('live-server');
+const StaticServer = require('static-server');
 const localtunnel = require('localtunnel');
 
 // Define options and flags
 const options = {
   port: { type: 'string', short: 'p', default: '8080' },
   host: { type: 'string', short: 'h', default: '127.0.0.1' },
-  root: { type: 'string', short: 'r' },
+  root: { type: 'string', short: 'r', default: '.' },
   sandbox: { type: 'string', short: 's', default: 'https://app.layer.com/submissions/generative/sandbox' },
+  tunnel_host: { type: 'string', short: 't', default: 'https://layerproxy.com' }
 };
 
 const { values } = parseArgs({ options });
-const rl = readline.createInterface({ input, output });
 
-const params = {
-  ...values,
-  open: false, // When false, it won't load your browser by default.
-  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-  middleware: [
-    function (req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
-      res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-      return next();
-    }
-  ]
-};
+var server = new StaticServer({
+  rootPath: values.root,            // required, the root of the server file tree
+  port: values.port,               // required, the port to listen
+  host: values.host,       // optional, defaults to any interface
+  name: 'serve-art',   // optional, will set "X-Powered-by" HTTP header
+  cors: '*',                // optional, defaults to undefined
+});
 
-function abort() {
-  console.log('Aborting...');
-  if (tunnel) tunnel.close();
-  process.exit();
-}
+// Wrap the server's `request` event to add headers
+server.on('request', (req, res) => {
+  res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+});
 
-const opened = import('open').then((mod) => mod.default);
-const controller = new AbortController();
-controller.signal.addEventListener('abort', abort)
 
 let tunnel;
-Promise.resolve(liveServer.start(params))
-  .then(async (server) => {
-    const address = server.address();
-    tunnel = await localtunnel({ port: address.port });
-
-    tunnel.on('close', () => {
-      // tunnels are closed
-      console.log('Tunnel closed');
+const opened = import('open').then((mod) => mod.default);
+new Promise(resolve => server.start(resolve))
+  .then(async () => {
+    tunnel = await localtunnel({
+      port: values.port,
+      host: values.tunnel_host
     });
-
-    console.log('You will need to provide localtunnel with the password.');
-    console.log(`Opening tunnel URL: ${tunnel.url}`);
-    return opened;
-  })
-  .then((open) => open(tunnel.url))
-  .then(() => rl.question('Ready to continue? (y)\n', controller))
-  .then(answer => {
-    if (`${answer}`.toLowerCase().trim().startsWith('n'))
-      return abort();
     return opened;
   })
   .then((open) => {
-    const url = `${params.sandbox}?url=${encodeURIComponent(tunnel.url)}`;
+    const url = `${values.sandbox}?url=${encodeURIComponent(tunnel.url)}`;
     console.log(`Sandbox URL: ${url}`);
     return open(url);
+  })
+  .catch((error) => {
+    if (error) console.error(error);
+    console.log('Aborting...');
+    if (tunnel) tunnel.close();
+    process.exit();
   });
